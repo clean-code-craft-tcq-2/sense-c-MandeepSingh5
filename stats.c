f2e2daae31b6b6511cee3b2f7455b15ffb71a48a
MandeepSingh5 <76588022+MandeepSingh5@users.noreply.github.com>
2021-10-18 22:09:13 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,10 +1,5 @@
 #include "stats.h"
-struct Stats
-{
-    float average;
-    float min;
-    float max;
-};
+
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     double sum = 0.0;

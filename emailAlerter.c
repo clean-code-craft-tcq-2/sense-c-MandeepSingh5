07749d40948404f7560571eb9924d8af777ebf82
MandeepSingh5 <76588022+MandeepSingh5@users.noreply.github.com>
2021-10-19 22:48:19 +0530

    Update emailAlerter.c

--- emailAlerter.c
+++ emailAlerter.c
@@ -1,1 +1,29 @@
 #include "emailAlerter.h"
+#include "stats.h"
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+  /* check whether max value is more than threshold
+  /*raises alerts when max is greater than threshold*/
+  const int number_of_alerts = 2
+  if(computedStats.max > maxThreshold)
+  {
+    for(int id = 0; id<number_of_alerts;id++)
+    {
+      (*alerters[id])(); /* calling email alert or led alert function based on id ( 0 ==> email alert ,  1==> led alert)
+    }
+  }
+  else
+  {
+    /* everything is under range, no alert is needed.*/
+  }
+}
+
+void emailAlertCallCount(void)
+{
+  ++emailAlertCallCount; /* increment the count for email alert */
+}
+void ledAlertCallCount(void)
+{
+  ++ledAlertCallCount;  /* increment the count for led alert */
+}

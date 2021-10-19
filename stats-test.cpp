a48a2bbcf8c68c063bfa9f6879498508dd794a27
MandeepSingh5 <76588022+MandeepSingh5@users.noreply.github.com>
2021-10-19 22:59:58 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -2,6 +2,7 @@
 
 #include "catch.hpp"
 #include "stats.h"
+#include "emailAlerter.h"
 
 #include <stdlib.h>
 #include <math.h>
@@ -20,14 +21,13 @@ TEST_CASE("average is NaN for empty array") {
     Stats computedStats = compute_statistics(0, 0);
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
-    int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    struct Stats computedStats = compute_statistics(numberset, setlength);
+    
     
     //Design the REQUIRE statement here.        
     REQUIRE(isnan(computedStats.average)==0);
     REQUIRE(isnan(computedStats.max)==0);
     REQUIRE(isnan(computedStats.min)==0);
-    #endif
+    
     
     
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
